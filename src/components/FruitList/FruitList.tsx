525436c8b13eb6bda18439461064f0c0fdae02e7
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-28 16:45:34 +0100

    The tests have been passed.

--- src/components/FruitList/FruitList.tsx
+++ src/components/FruitList/FruitList.tsx
@@ -4,6 +4,15 @@ import './FruitList.css';
 
 export const FruitList = () => {
     return (
-        <div data-testId="fruit-list-wrapper">Fruit List</div>
+        <div className="fruit-list" data-testId="fruit-list-wrapper">
+            <div className="fruit-list__left">
+                <h2>Fruit I need to buy</h2>
+                <div>List A</div>
+            </div>
+            <div className="fruit-list__right">
+                <h2>Fruit I have</h2>
+                <div>list B</div>
+            </div>
+        </div>
     );
 }
