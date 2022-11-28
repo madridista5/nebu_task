c97426454316709db98ee170a70b0f5181eacbd3
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-28 15:38:25 +0100

    Test: Header Component contains header and h1.

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,4 +1,4 @@
-import {cleanup, render} from "@testing-library/react";
+import {cleanup, render, screen} from "@testing-library/react";
 import {Header} from "../Header/Header";
 
 afterEach(() => {
@@ -10,4 +10,16 @@ describe('Header Component', () => {
     test('renders Header Component', () => {
         render(<Header/>);
     });
+
+    test('Header Component contains header and h1', () => {
+        // Arrange
+        render(<Header/>);
+        const headerElement = screen.getByTestId('header');
+        const h1Element = screen.getByTestId('h1');
+
+        // Assert
+        expect(headerElement).toContainElement(h1Element);
+        expect(h1Element).toBeInTheDocument();
+    });
+
 });
