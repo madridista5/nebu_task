81cf0527bae5a1d5c5da99f57eba8aa45af1297b
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-28 16:35:22 +0100

    Tests: render h2 element with string: "Fruit I need to buy" and h2 element with string: Fruit I have.

--- src/components/__tests__/fruitList.test.tsx
+++ src/components/__tests__/fruitList.test.tsx
@@ -1,4 +1,4 @@
-import {cleanup, render} from "@testing-library/react";
+import {cleanup, render, screen} from "@testing-library/react";
 import {FruitList} from "../FruitList/FruitList";
 
 afterEach(() => {
@@ -9,5 +9,17 @@ describe('Fruit List Component', () => {
 
     test('renders FruitList Component', () => {
         render(<FruitList/>);
+    });
+
+    test('renders h2 element with string: "Fruit I need to buy"', () => {
+        render(<FruitList/>);
+        const h2Element = screen.getByText('Fruit I need to buy');
+        expect(h2Element).toBeInTheDocument();
+    });
+
+    test('renders h2 element with string: "Fruit I have"', () => {
+        render(<FruitList/>);
+        const h2Element = screen.getByText('Fruit I have');
+        expect(h2Element).toBeInTheDocument();
     });
 });
